ca42234b079817cf055d17ad0411f2b6218082d2
Topher Brown <t.brown@wordstream.com>
2016-05-20 21:54:51 -0400

    update test script to save to logger

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 # undo changes to ws directory
 pushd ~/dev/wordstream
-git checkout -f
+git checkout -f &> /dev/null
 popd
 
 # run our script
-perl property_conversion.pl
+perl property_conversion.pl &> log.txt || true && tail -n 30 log.txt
